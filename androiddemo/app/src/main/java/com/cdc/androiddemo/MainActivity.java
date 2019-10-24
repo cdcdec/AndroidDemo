f056ebacfb72616c7c502a5bf6d10a130f9dc92d
cdc <420209677@qq.com>
2019-10-24 15:23:19 +0800

    当行 垂直或者水平的跑马灯

--- androiddemo/app/src/main/java/com/cdc/androiddemo/MainActivity.java
+++ androiddemo/app/src/main/java/com/cdc/androiddemo/MainActivity.java
@@ -1,18 +1,34 @@
 package com.cdc.androiddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import com.cdc.mlog.MLog;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    String a=null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter.Builder(this, getSupportFragmentManager())
+                .add("Common", new CommonFragment())
+                .add("RecyclerView", new RecyclerViewFragment())
+                .build();
+        viewPager.setAdapter(adapter);
+        viewPager.setOffscreenPageLimit(1);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
     }
 }
---
+++ androiddemo/marqueeview/src/main/java/com/sunfusheng/marqueeview/IMarqueeItem.java
@@ -0,0 +1,7 @@
+package com.sunfusheng.marqueeview;
+/**
+ * @author by sunfusheng on 2019-04-25
+ */
+public interface IMarqueeItem {
+    CharSequence marqueeMessage();
+}

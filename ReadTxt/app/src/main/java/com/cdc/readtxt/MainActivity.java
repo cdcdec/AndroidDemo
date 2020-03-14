ad8fc4c9fe0afbf4a7ed41b010a30c9a87a97a40
cdc <420209677@qq.com>
2020-03-14 18:07:33 +0800

    yueduqi

---
+++ ReadTxt/app/src/main/java/com/cdc/readtxt/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cdc.readtxt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //https://github.com/bifan-wei/HwTxtReader
+    }
+}
